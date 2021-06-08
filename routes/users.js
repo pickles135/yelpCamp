be75851bcdcff4b0073e411b481f0f43d878ee9d
tcho <teresacho135@gmail.com>
2021-06-07 22:40:04 -0700

    creates login form and POST route for login

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/users');
 
@@ -21,5 +22,16 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('register')
     }
 }));
+
+//login form
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+//login form submit
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', "Welcome Back!");
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
